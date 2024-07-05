778d91dba7901817988dbdf57c42dfd681234644
syeopite <syeopite@syeopite.dev>
2024-07-05 15:15:24 -0700

    Fix casting of JSON::Any

--- src/cryyjson.cr
+++ src/cryyjson.cr
@@ -16,7 +16,7 @@ module Cryyjson
     return JSON::Any.new(object)
   end
 
-  private def self.parse_object(object)
+  private def self.parse_object(object) : Hash(String, JSON::Any)
     iterator = LibYYJSON::ObjIter.new()
     LibYYJSON.yyjson_obj_iter_init(object, pointerof(iterator))
 
@@ -27,25 +27,25 @@ module Cryyjson
       raw_value = LibYYJSON.yyjson_obj_iter_get_val(key)
       processed_value = self.parse_json_value(raw_value)
 
-      json_object[str_key] = JSON::Any.new(processed_value)
+      json_object[str_key] = processed_value
     end
 
     return json_object
   end
 
-  private def self.parse_array(arr)
+  private def self.parse_array(arr) : Array(JSON::Any)
     iterator = LibYYJSON::ArrIter.new()
     LibYYJSON.yyjson_arr_iter_init(arr, pointerof(iterator))
 
     json_array = [] of JSON::Any
     while (value = LibYYJSON.yyjson_arr_iter_next(pointerof(iterator)))
-      json_array << JSON::Any.new(self.parse_json_value(value))
+      json_array << self.parse_json_value(value)
     end
 
     return json_array
   end
 
-  private def self.parse_json_value(value_pointer : Pointer(LibYYJSON::Val)) : JSON::Any::Type
+  private def self.parse_json_value(value_pointer : Pointer(LibYYJSON::Val)) : JSON::Any
     # Identify value type
     value = value_pointer.value
     value_type = LibYYJSON::YYJSONType.from_value(value.tag & LibYYJSON::JSONValueMask::TYPE_MASK.value)
@@ -53,24 +53,26 @@ module Cryyjson
 
     case value_type
     when .null?
-      return nil
+      return JSON::Any.new(nil)
     when .bool?
-      return value_subtype.true? ? true : false
+      return JSON::Any.new(value_subtype.true? ? true : false)
     when .num?
       case value_subtype
       when .uint?
-        return Int64.new(value.uni.u64)
+        return JSON::Any.new(Int64.new(value.uni.u64))
       when .sint?
-        return Int64.new(value.uni.i64)
+        return JSON::Any.new(Int64.new(value.uni.i64))
       when .real?
-        return Float64.new(value.uni.f64)
+        return JSON::Any.new(Float64.new(value.uni.f64))
+      else
+        raise JSON::Error.new("Invalid number subtype")
       end
     when .str?
-      return String.new(value.uni.str)
+      return JSON::Any.new(String.new(value.uni.str))
     when .arr?
-      return self.parse_array(value_pointer)
+      return JSON::Any.new(self.parse_array(value_pointer))
     when .obj?
-      return self.parse_object(value_pointer)
+      return JSON::Any.new(self.parse_object(value_pointer))
     else
       raise JSON::Error.new("Invalid type")
     end
