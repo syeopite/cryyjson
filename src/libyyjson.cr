fccf0294cd4e6058c886517f73474a44c558558f
syeopite <syeopite@syeopite.dev>
2024-07-05 13:11:37 -0700

    Bind functions to iterate through JSON objects

--- src/libyyjson.cr
+++ src/libyyjson.cr
@@ -1,4 +1,4 @@
-@[Link("yyjson")]
+@[Link(ldflags: "#{__DIR__}/ext/libyyjson.a")]
 lib LibYYJSON
   struct Document
     root : Val*
@@ -28,5 +28,15 @@ lib LibYYJSON
     pos : LibC::SizeT
   end
 
-  fun yyjson_read_opts(dat : LibC::Char*, len : LibC::SizeT, flg : UInt32, alc : Void*, err : YYJSONReadError*) : Document*
+  struct ObjIter
+    idx : LibC::SizeT # Next key's index
+    max : LibC::SizeT # Max key index
+    cur : Val # Value of the next key
+    obj : Val # The object being iterated
   end
+
+  fun yyjson_read_opts(dat : LibC::Char*, len : LibC::SizeT, flg : UInt32, alc : Void*, err : YYJSONReadError*) : Document*
+  fun yyjson_obj_iter_init(obj : Val*, iter : ObjIter*) : Bool
+  fun yyjson_obj_iter_next(iter : ObjIter*) : Val*
+  fun yyjson_obj_iter_get_val(key : Val*) : Val*
+end
