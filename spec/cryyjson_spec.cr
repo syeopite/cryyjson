a8970cf16db543fb365824a5cbb56680920def74
syeopite <syeopite@syeopite.dev>
2025-05-23 15:10:08 -0700

    Add basic tests for cryyjson

--- spec/cryyjson_spec.cr
+++ spec/cryyjson_spec.cr
@@ -1,9 +1,34 @@
 require "./spec_helper"
 
+# Taken from Crystal Stdlib
+private def it_parses(string, expected_value, file = __FILE__, line = __LINE__)
+  it "parses #{string}", file, line do
+    Cryyjson.parse(string).raw.should eq(expected_value)
+  end
+end
+
 describe Cryyjson do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it_parses "{}", {} of String => JSON::Any
+  it_parses %({"foo": 1}), {"foo" => 1}
+  it_parses %({"foo": 1, "bar": 1.5}), {"foo" => 1, "bar" => 1.5}
+  it_parses %({"fo\\no": 1}), {"fo\no" => 1}
+
+  it "parses nested json" do
+    experimental = {
+      "key" => "value",
+      "obj" => {
+        "arr" => [
+          {"a" => "b"},
+          {"b" => "c"},
+        ],
+        "str" => "str",
+        "num" => 1234
+      },
+      "key2" => "key2"
+    }
+
+    Cryyjson.parse(experimental.to_json).raw.should eq(experimental)
   end
 end
